b1ff9523d0c12bf0f5bc734c75d7d802b6d95fd5
Ihab <mourad3180@hotmail.fr>
2020-01-21 10:54:45 -0500

    Project of ScriptablObjects

---
+++ Assets/2. Character Stat System/Scripts/Scriptable Objects/ItemPickUp_SO.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemTypeDefinitions
+{
+    HEALTH,
+    WEALTH,
+    MANA,
+    WEAPON,
+    ARMOR,
+    BUFF,
+    EMPTY
+}
+
+public enum ItemArmorSubType
+{
+    None,
+    Head,
+    Chest,
+    Hand,
+    Legs,
+    Boots
+}
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Spawnable item / new Pick-up", order = 1)]
+public class ItemPickUp_SO : ScriptableObject
+{
+    public ItemTypeDefinitions itemType = ItemTypeDefinitions.HEALTH;
+    public ItemArmorSubType itemArmorSubType = ItemArmorSubType.None;
+    public int itemAmount = 0;
+}
